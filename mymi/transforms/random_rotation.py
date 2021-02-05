Fix random rotation

mymi/transforms/random_rotation.py
@@ -4,18 +4,18 @@ import numpy as np
 from scipy.ndimage import rotate
 
 class RandomRotation:
-    def __init__(self, range_axial=None, range_coronal=None, range_sagittal=None, fill=0, p=1.0):
+    def __init__(self, range_x=None, range_y=None, range_z=None, fill=0, p=1.0):
         """
         kwargs:
-            range_axial: a (min, max) tuple describing angle range in degrees about the axial axis.
-            range_coronal: a (min, max) tuple describing angle range in degrees about coronal axis.
-            range_sagittal: a (min, max) tuple describing angle range in degrees about sagittal axis.
+            range_x: a (min, max) tuple describing angle range in degrees about the axial axis.
+            range_y: a (min, max) tuple describing angle range in degrees about coronal axis.
+            range_z: a (min, max) tuple describing angle range in degrees about sagittal axis.
             fill: values with which to fill new pixels.
             p: the probability that the rotation will be applied.
         """
-        self.range_axial = range_axial
-        self.range_coronal = range_coronal
-        self.range_sagittal = range_sagittal
+        self.range_x = range_x
+        self.range_y = range_y
+        self.range_z = range_z
         self.fill = fill
         self.p = p
 
@@ -59,9 +59,9 @@ class RandomRotation:
         applied = True if np.random.binomial(1, self.p) else False
 
         # Determine angles.
-        angle_x = np.random.uniform(*self.range_axial) if self.range_axial is not None else None
-        angle_y = np.random.uniform(*self.range_coronal) if self.range_coronal is not None else None
-        angle_z = np.random.uniform(*self.range_sagittal) if self.range_sagittal is not None else None
+        angle_x = np.random.uniform(*self.range_x) if self.range_x is not None else None
+        angle_y = np.random.uniform(*self.range_y) if self.range_y is not None else None
+        angle_z = np.random.uniform(*self.range_z) if self.range_z is not None else None
         angles = (angle_x, angle_y, angle_z)
         
         return applied, angles 