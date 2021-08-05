c41b062eef682e3889e3b2f4130b4db7210261c3
Brett Clark <baclark@spartan-login3.hpc.unimelb.edu.au>
2021-08-05 22:12:03 +1000

    Fix metric imports

--- mymi/metrics/__init__.py
+++ mymi/metrics/__init__.py
@@ -1,4 +1,4 @@
 import torch
 
-from .dice import batch_mean_dice, dice, sitk_batch_mean_dice
+from .dice import batch_mean_dice, dice
 from .hausdorff_distance import batch_mean_hausdorff_distance, sitk_batch_mean_hausdorff_distance, sitk_hausdorff_distance
