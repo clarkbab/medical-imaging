a9ff52efb5ffb919b598f2dec2be24c35e827cf7
Brett Clark <baclark@spartan-login2.hpc.unimelb.edu.au>
2021-08-27 12:33:34 +1000

    Add multiple OARs to processed datasets

--- mymi/reporting/dataset/raw/dicom.py
+++ mymi/reporting/dataset/raw/dicom.py
@@ -3,9 +3,29 @@ import os
 from mymi import dataset as ds
 from mymi import types
 
-def create_region_count_report(dataset: str) -> None:
+def create_region_count_report(
+    dataset: str,
+    clear_cache: bool = False,
+    regions: types.PatientRegions = 'all') -> None:
+    # List regions.
     set = ds.get(dataset, type_str='dicom')
-    regions_df = set.list_regions()
+    regions_df = set.list_regions(clear_cache=clear_cache)
+
+    # Filter on requested regions.
+    def filter_fn(row):
+        if type(regions) == str:
+            if regions == 'all':
+                return True
+            else:
+                return row['region'] == regions
+        else:
+            for region in regions:
+                if row['region'] == region:
+                    return True
+            return False
+    regions_df = regions_df[regions_df.apply(filter_fn, axis=1)]
+
+    # Generate counts report.
     counts = regions_df.groupby('region').count()['patient-id']
     filename = 'region-count.csv'
     filepath = os.path.join(set.path, 'reports', filename)
@@ -19,21 +39,22 @@ def get_region_overlap(
     set = ds.get(dataset, type_str='dicom')
     regions_df = set.list_regions(clear_cache=clear_cache) 
     regions_df = regions_df.drop_duplicates()
-    regions_df['count'] = 1
-    pivot_df = regions_df.pivot(index='patient-id', columns='region', values='count')
 
+    # Filter on requested regions.
     def filter_fn(row):
         if type(regions) == str:
             if regions == 'all':
                 return True
             else:
-                return row[regions] == 1
+                return row['region'] == regions
         else:
             keep = True
             for region in regions:
-                if row[region] != 1:
+                if row['region'] != region:
                     keep = False
-            return keep
+    regions_df = regions_df[regions_df.apply(filter_rn, axis=1)]
 
-    pat_df = pivot_df[pivot_df.apply(filter_fn, axis=1)]
+    # Pivot
+    regions_df['count'] = 1
+    pivot_df = regions_df.pivot(index='patient-id', columns='region', values='count')
     return len(pat_df) 
