e0c5c7a9e1dc7e3a8f7a13a9122761c70f394036
Brett Clark <baclark@spartan-login3.hpc.unimelb.edu.au>
2021-09-10 23:29:13 +1000

    Skip RTSTRUCT series if not valid

--- mymi/dataset/raw/dicom/dicom_patient.py
+++ mymi/dataset/raw/dicom/dicom_patient.py
@@ -53,7 +53,26 @@ class DICOMPatient:
             raise ValueError(f"Expected 1 RTSTRUCT, got '{len(rtstruct_series)}' for patient '{self}'.")
         
         # Set default RTSTRUCT series.
-        self._default_rtstruct_series = RTSTRUCTSeries(self, rtstruct_series[0], ct_from=ct_from, region_map=region_map)
+        msgs = []
+        for s in rtstruct_series:
+            try:
+                series = RTSTRUCTSeries(self, rtstruct_series[0], ct_from=ct_from, region_map=region_map)
+            except ValueError as e:
+                msg = f"Error encountered when loading RTSTRUCT series '{s}', skipping."
+                error_msg = f"Error: {e}"
+                logging.error(msg)
+                logging.error(error_msg)
+                msgs.append(msg)
+                msgs.append(error_msg)
+
+        # Raise error if no valid series was found.
+        if series is None:
+            msg = f"No valid RTSTRUCT series found for patient '{patient}', tried '{len(rtstruct_series)}'."
+            msgs.insert(0, msg)
+            raise ValueError('\n'.join(msgs))
+                
+
+self._default_rtstruct_series
 
     def cache_key(self) -> str:
         return self._global_id
