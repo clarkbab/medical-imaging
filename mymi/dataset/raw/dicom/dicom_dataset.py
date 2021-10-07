103c1ff0213b7fe6287858c783381c9985193cab
Brett Clark <baclark@spartan-login3.hpc.unimelb.edu.au>
2021-10-07 11:36:29 +1100

    Removed debugging filter

--- mymi/dataset/raw/dicom/dicom_dataset.py
+++ mymi/dataset/raw/dicom/dicom_dataset.py
@@ -110,8 +110,6 @@ class DICOMDataset(Dataset):
         else:
             path = os.path.join(self._path, 'hierarchy', 'data')
         pats = list(sorted(os.listdir(path)))
-        
-        pats = pats[:5]
 
         # Filter by 'regions'.
         pats = list(filter(self._filter_patient_by_regions(regions), pats))
